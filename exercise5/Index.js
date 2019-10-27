95fe96fcd68eeeda3ce553efb2489e60591de4f3
izaak svec <slavi.svec@gmail.com>
2019-10-27 11:30:08 +1100

    add of to do items

--- exercise5/Index.js
+++ exercise5/Index.js
@@ -1,12 +1,15 @@
-// From scratch, initialize the React app
-// Render an <App /> component
-// Create the <App /> component from scratch
-// Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
-// like you're making a todo list with some hard-coded items on it
+/*
+Time to have fun styling! But first things first:
+
+1. Change the input/p combo below to be a new component called <TodoItem />. <TodoItem /> (for now) will just have the same displayed data below (every todo item is the same) hardcoded inside of it. (We'll learn soon how to make the TodoItem more flexible)
+
+2. Style up the page however you want! You're welcome to use regular CSS (in the CSS file) or inline styles, or both!
+*/
+
 
 import Rect from "react"
 import ReactDom   from "react-dom"
 
 import App from "./App"
 
-ReactDom.render(<App />,document.getElementById("root")) 
+ReactDom.render(<App />,document.getElementById("root"))
---
+++ exercise5/Todoitem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function todoItem() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p> Placeholder text here </p>
+        </div>
+
+    )
+}
+
+
+export default TodoItem
