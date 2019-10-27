2eed153b90b6e190c2c9053fed2fbef237f885ea
izaak svec <slavi.svec@gmail.com>
2019-10-27 13:41:38 +1100

    Question 6 jokes

---
+++ exercise6/Index6.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App6 from  './App6 '
+
+ReactDOM.render(<App6 />,document.getElementById("root))")
---
+++ exercise6/Jokes.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Jokes (props) {
+    return (
+        <div>
+            <h3> Question { props.question } </h3>
+            <h3> answers </h3>
+        </div>
+    )
+} 
+export defalut Jokes
